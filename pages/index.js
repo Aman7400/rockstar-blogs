91369bbf4b08f3fd22b29997ca3af7286f55b09c
Aman Shukla <amanks7400@gmail.com>
2022-08-18 13:12:46 +0530

    blog post v1

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import { Box, Button, Container, Grid, GridItem, Heading, Image, Input, InputGroup, InputLeftElement, Text, useTheme, useToast } from '@chakra-ui/react'
+import { Badge, Box, Button, Container, Grid, GridItem, Heading, Image, Input, InputGroup, InputLeftElement, Stack, Text, useTheme, useToast } from '@chakra-ui/react'
 import Link from 'next/link'
 import React from 'react'
 import { MdBuild, MdCall } from "react-icons/md"
+import { FaExternalLinkAlt } from "react-icons/fa"
 
 export default function Home() {
   const theme = useTheme()
@@ -50,47 +51,76 @@ function HeroSection() {
 
 function RecentBlogPosts() {
   return (
-    <Container maxW="container.xl">
-      <Text fontSize="xl" >
+    <Container maxW="container.xl" my={16}>
+      <Text fontSize="xl" mb={4} >
         Recent Blog Posts
       </Text>
       <Grid
-        templateColumns='repeat(5, 1fr)'
+        templateRows='repeat(3, 1fr)'
+        templateColumns='repeat(8, 1fr)'
         gap={4} width="container.xl" >
-        <GridItem colSpan={3}>
-          <Box p={5} shadow={"md"} borderWidth='1px'>
-            <Image alt='Dan Abramov' objectFit='cover'
-              src='https://picsum.photos/800/400' />
-            <Heading fontSize='xl'>Blog Recent 1</Heading>
-            <Text mt={4}>lorem ipsum dolor sit</Text>
-            <Link href={`/blogs/recent1`}>Read More</Link>
-          </Box>
+        <GridItem rowSpan={3} colSpan={5}>
+          <RecentBlogCard id={1} />
         </GridItem>
-        <GridItem colSpan={2} >
-          <Box p={5} shadow={"md"} borderWidth='1px'>
-            <Image alt='Dan Abramov' objectFit='cover'
-              src='https://picsum.photos/600/180' />
-            <Heading fontSize='xl'>Blog Recent 2</Heading>
-            <Text mt={4}>lorem ipsum dolor sit</Text>
-            <Link href={`/blogs/recent2`}>Read More</Link>
-          </Box>
-          <Box marginBlock={4} p={5} shadow={"md"} borderWidth='1px'>
-            <Image alt='Dan Abramov' objectFit='cover'
-              src='https://picsum.photos/600/180' />
-            <Heading fontSize='xl'>Blog Recent 3</Heading>
-            <Text mt={4}>lorem ipsum dolor sit</Text>
-            <Link href={`/blogs/recent3`}>Read More</Link>
-          </Box>
+        <GridItem rowSpan={1} colSpan={3} >
+          <RecentBlogCard id={2} orientation="row" />
         </GridItem>
-
+        <GridItem rowSpan={1} colSpan={3} >
+          <RecentBlogCard id={3} orientation="row" />
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={3} >
+          <RecentBlogCard id={4} orientation="row" />
+        </GridItem>
       </Grid>
-      {/* Blog Wrapper */}
-      {/* Latest #1 */}
-      {/* Latest #2 */}
-      {/* Latest #3 */}
     </Container>
   )
 }
+
+function RecentBlogCard({ id, orientation = "column" }) {
+  let imgSize = { width: 800, height: 400 }
+  let textSize = { heading: "2xl", desc: "lg" }
+  let btnSize = { readMore: "lg", badge: "" }
+  let description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Laoreet suspendisse interdum consectetur libero id.';
+  let textContainer = {
+    padding: 5
+  }
+  if (orientation === "row") {
+    imgSize.width = 240;
+    imgSize.height = 140;
+    textSize.heading = "lg";
+    textSize.desc = "x-small";
+    btnSize.readMore = "xs";
+    textContainer.padding = 2.5
+    description = description.substring(0,96) + "..."
+
+  }
+
+  
+
+  return (
+    <Box display="flex" flexDirection={orientation} shadow={"md"} borderWidth='1px'>
+      <Image alt='Dan Abramov' objectFit='cover'
+        src={`https://picsum.photos/${imgSize.width}/${imgSize.height}`} />
+
+      <Box p={textContainer.padding}>
+        <Stack marginBlock={2} direction='row' flexWrap="wrap">
+          <Badge >Default</Badge>
+          <Badge colorScheme='green'>Success</Badge>
+          <Badge colorScheme='red'>Removed</Badge>
+          <Badge colorScheme='purple'>New</Badge>
+        </Stack>
+        <Heading fontSize={textSize.heading}>Blog Recent {id}</Heading>
+        <Text color="gray.500" fontSize={textSize.desc} mt={1}>{description}</Text>
+        <Button size={btnSize.readMore} variant="ghost" colorScheme="red" rightIcon={<FaExternalLinkAlt />}>
+          <Link href={`/blogs/recent${id}`}>
+            Read More
+          </Link>
+        </Button>
+      </Box>
+    </Box>
+  )
+}
+
 function AllBlogPosts() {
   const [mouseOver, setMouseOver] = React.useState(false)
   return (

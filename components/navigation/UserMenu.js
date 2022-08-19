10d5df823a012df8d7a518b7f154f680d825dc4c
Aman Shukla <amanks7400@gmail.com>
2022-08-19 08:15:59 +0530

    Update UserMenu.js

--- components/navigation/UserMenu.js
+++ components/navigation/UserMenu.js
@@ -1,23 +1,25 @@
 import { Avatar, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import Link from 'next/link'
 import React from 'react'
+import { BsFillPersonFill } from "react-icons/bs"
+import {AiTwotoneSetting,AiOutlineLogout} from "react-icons/ai"
 
 const UserMenu = ({handleLogout}) => {
     return (
         <Menu>
             <MenuButton as={Avatar} size='sm' cursor='pointer' src='https://bit.ly/dan-abramov' />
             <MenuList>
-                <MenuItem>
+                <MenuItem  icon={<BsFillPersonFill />}>
                     <Link href={"/user/profile"}>
                         Profile
                     </Link>
                 </MenuItem>
-                <MenuItem>
+                <MenuItem icon={<AiTwotoneSetting />}>
                     <Link href={"/user/settings"}>
                         Settings
                     </Link>
                 </MenuItem>
-                <MenuItem onClick={() => handleLogout()}>Logout</MenuItem>
+                <MenuItem icon={<AiOutlineLogout />} onClick={() => handleLogout()}>Logout</MenuItem>
 
             </MenuList>
         </Menu>
